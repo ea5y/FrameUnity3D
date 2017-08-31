3a2e5c5d00631a16165a39c4ab7acff9dc8260c1
ea5y <gopiny@live.com>
2017-08-31 16:08:13 +0800

    add

---
+++ Assets/Scripts/Controller/PlayerCameraController.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Easy.Unity
+{
+    public class PlayerCameraController : MonoBehaviour
+    {
+        public GameObject Player;
+        public ETCTouchPad TouchPad;
+        private Vector3 _offset;
+
+        public GameObject TestPrefab;
+
+        private bool _isOffsetChanging = false;
+
+        private void Awake()
+        {
+            _offset = Player.transform.position - transform.position;
+
+            TouchPad.onMove.AddListener(OnMove);
+            TouchPad.onMoveStart.AddListener(OnMoveStart);
+            TouchPad.onMoveSpeed.AddListener(OnMoveSpeed);
+            TouchPad.onMoveEnd.AddListener(OnMoveEndTouchPad);
+
+            TouchPad.onTouchStart.AddListener(OnTouchStart);
+            TouchPad.onTouchUp.AddListener(OnTouchUp);
+
+            TouchPad.OnPressDown.AddListener(OnPressDown);
+            TouchPad.OnPressLeft.AddListener(OnPressLeft);
+            TouchPad.OnPressRight.AddListener(OnPressRight);
+            TouchPad.OnPressUp.AddListener(OnPressUp);
+
+            TouchPad.OnDownDown.AddListener(OnDownDown);
+            TouchPad.OnDownLeft.AddListener(OnDownLeft);
+            TouchPad.OnDownRight.AddListener(OnDownRight);
+            TouchPad.OnDownUp.AddListener(OnDownUp);
+        }
+
+        private void LateUpdate()
+        {
+            if (_isOffsetChanging)
+                return;
+            if(Player != null)
+            {
+                transform.position = Player.transform.position - _offset;
+            }
+        }
+
+        private void OnTouchStart()
+        {
+            _isOffsetChanging = true;
+            Debug.Log(string.Format("TouchStart"));
+        }
+
+        private void OnTouchUp()
+        {
+            Debug.Log(string.Format("TouchUp"));
+            _isOffsetChanging = false;
+        }
+
+        private void OnMoveStart()
+        {
+            Debug.Log(string.Format("MoveStart"));
+        }
+
+        private void OnMove(Vector2 vec)
+        {
+            //Debug.Log(string.Format("Move"));
+            transform.RotateAround(Player.transform.position, Vector3.up, vec.x * 30 * Time.deltaTime);
+            _offset = Player.transform.position - transform.position;
+        }
+
+        private void OnMoveSpeed(Vector2 vec)
+        {
+            Debug.Log(string.Format("MoveSpeed"));
+        }
+
+        private void OnMoveEndTouchPad()
+        {
+            Debug.Log(string.Format("MoveEnd"));
+        }
+
+        private void OnPressUp()
+        {
+            Debug.Log(string.Format("PressUp"));
+        }
+
+        private void OnPressRight()
+        {
+            Debug.Log(string.Format("PressRight"));
+        }
+
+        private void OnPressDown()
+        {
+            Debug.Log(string.Format("PressDown"));
+        }
+
+        private void OnPressLeft()
+        {
+            Debug.Log(string.Format("PressLeft"));
+        }
+
+        private void OnDownUp()
+        {
+            Debug.Log(string.Format("DownUp"));
+        }
+
+        private void OnDownRight()
+        {
+            Debug.Log(string.Format("DownRight"));
+        }
+
+        private void OnDownDown()
+        {
+            Debug.Log(string.Format("DownDown"));
+        }
+
+        private void OnDownLeft()
+        {
+            Debug.Log(string.Format("DownLeft"));
+        }
+    }
+}
+
---
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public ETCJoystick Joystick;
+    public Camera MainCamera;
+    public GameObject ViewPoint;
+
+    void Start ()
+    {
+        Joystick.onMoveStart.AddListener(OnMoveStart);
+        Joystick.onMove.AddListener(OnMove);
+        Joystick.onMoveEnd.AddListener(OnMoveEnd);
+	}
+
+    public void OnMoveStart()
+    {
+    }
+
+    private float CollationPlayerForward()
+    {
+        var vec1 = transform.forward.normalized;
+        var vec2 = MainCamera.transform.forward.normalized;
+        var dot = Vector3.Dot(vec1, vec2);
+        var rotate = Mathf.Acos(dot) * Mathf.Rad2Deg;
+        if (!float.IsNaN(rotate))
+        {
+            Debug.Log(string.Format("Rotate: {0}", rotate));
+            transform.Rotate(Vector3.up, rotate);
+        }
+        return rotate;
+    }
+
+    private void CreateRotateMatrixForViewPoint(float angle)
+    {
+        _matrix = Matrix4x4.identity;
+        _matrix.SetTRS(Vector3.zero, Quaternion.Euler(0, angle, 0), Vector3.one);
+    }
+
+    public void OnMove(Vector2 v)
+    {
+        Vector4 offset = new Vector4(v.x, 0, v.y, 1);
+        transform.LookAt(MainCamera.transform.TransformVector(offset) + transform.position);
+        transform.Translate(transform.forward * Time.deltaTime * 2, Space.World);
+    }
+
+    public void OnMoveEnd()
+    {
+
+    }
+
+    private Mesh _mesh;
+    public Vector3[] vertices;
+    public int[] triangle;
+
+    private Vector4 _startPos;
+    private Matrix4x4 _matrix;
+
+    public float _x;
+    public float _y;
+    public float _z;
+
+    private Vector2 _vec = new Vector2(1, 1);
+    private void Awake()
+    {
+        _mesh = new Mesh();
+        _mesh.vertices = vertices;
+        _mesh.triangles = triangle;
+        GetComponent<MeshFilter>().mesh = _mesh;
+
+        _startPos = new Vector4(transform.position.x, transform.position.y, transform.position.z, 1);
+    }
+
+    private void Update()
+    {
+        //Translate(_vec);
+    }
+
+    private void Translate(Vector2 vec)
+    {
+        _matrix = Matrix4x4.identity;
+
+        _matrix.SetTRS(new Vector3(_x + vec.x, _y, _z + vec.y) * 0.2f, Quaternion.identity, Vector3.one);
+
+        var t = _matrix * _startPos;
+        transform.position = new Vector3(t.x, t.y, t.z);
+    }
+}
